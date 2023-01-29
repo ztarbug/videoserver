5ea3370a62daa885ce1641c50c7bec5f82312dbe
ztarbug <kronprinzfriedrich@gmx.de>
2023-01-29 16:16:58 +0100

    fixed datastructure for registered clients

--- python/sampleServer.py
+++ python/sampleServer.py
@@ -19,31 +19,33 @@ import videoconnector_pb2_grpc
 
 class VideoServer(videoconnector_pb2_grpc.VideoConnectorServicer):
 
-    clients = {}
-    number_client = 0
-
     def __init__(self):
         logging.info("server created")
+        self.clients = {}
+        self.number_client = 0
 
     def RegisterClient(self, request, context):
-        print("register new client")
+        print("register new client " + request.hostname)
 
-        cd = clientdata.ClientData()
-        cd.client_hostname = request.hostname
-        cd.client_id = self.number_client        
-        cd.command_queue.append(videoconnector_pb2.CommandType.GetSourceInfo)
-        cd.command_queue.append(videoconnector_pb2.CommandType.GetImage)        
-        self.clients[cd.client_id] = cd
+        cmd_queue = []
+        cmd_queue.append(videoconnector_pb2.CommandType.GetSourceInfo)
+        cmd_queue.append(videoconnector_pb2.CommandType.GetImage)        
+        #cmd_queue.append(videoconnector_pb2.CommandType.StopAndShutdown)         
+
+        cd = clientdata.ClientData(request.hostname, self.number_client, cmd_queue)
+
+        self.clients[self.number_client] = cd
+
         self.number_client += 1
-        print("number of registered clients " + str(self.number_client))
+        print("number of registered clients " + str(len(self.clients)))
 
         context.set_code(grpc.StatusCode.OK)
         context.set_details('Executed')
-        resp = videoconnector_pb2.RegisterResponse()
+        resp = videoconnector_pb2.RegisterResponse(id = cd.client_id)
         return resp
 
     def UnRegisterClient(self, request, context):
-        print("unregister client")
+        print("unregister client " + request.hostname)
 
         del self.clients[request.client_id]
 
@@ -56,12 +58,11 @@ class VideoServer(videoconnector_pb2_grpc.VideoConnectorServicer):
         return resp
 
     def GetCommand(self, request, context):
-        print("GetCommand")
+        print("GetCommand " + str(request.client_id) + "/" + request.connectorHostname)
         context.set_code(grpc.StatusCode.OK)
         context.set_details('Executed')
 
-        ts = Timestamp()
-
+        ts = Timestamp()    
         resp = videoconnector_pb2.CommandList(serverTimestamp = ts, commands = self.clients[request.client_id].command_queue)
         self.clients[request.client_id].command_queue = []
 
@@ -73,7 +74,8 @@ class VideoServer(videoconnector_pb2_grpc.VideoConnectorServicer):
         context.set_details('Executed')
 
         image_bytes = base64.b64decode(request.image)
-        self.SaveImage(image_bytes, request.client_id)        
+        self.SaveImage(image_bytes, request.client_id)
+        self.clients[request.client_id].command_queue.append(videoconnector_pb2.CommandType.GetImage)
 
         ts = Timestamp()
         resp = videoconnector_pb2.ServerAckResponse(serverTimestamp = ts)
@@ -99,10 +101,6 @@ class VideoServer(videoconnector_pb2_grpc.VideoConnectorServicer):
 
 
 def serve():
-    cd = clientdata.ClientData()
-    cd.client_hostname = "sdgdfgdf"
-    cd.client_id = 1
-    cd.command_queue = []
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options = [
             ('grpc.max_send_message_length', -1),
             ('grpc.max_receive_message_length', -1)
